9a7088d472e98a3a9170326cc1a23a2b09d8089f
Susan Perkins <suziep1@me.com>
2011-05-08 17:17:00 -0700

    First commit after initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oasis::Application.config.secret_token = '48a619adea82da7e1ef15b924d9421c657767dd9407b6eca0799c941ad219210ddbdeb2cb3835dea8d1ac7593607546ae7e80c7d97ce9e76f1b371400f5461d8'
